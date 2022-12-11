6044b96de5d1b9edca802d4fa99b968679be7933
sunglasses <fred@secondlast.com>
2022-12-12 00:03:26 +1100

    d

---
+++ src/core/js/guide.js
@@ -0,0 +1,16 @@
+const weapons = document.getElementById('weapons')
+const tools = document.getElementById('tools')
+const placeables = document.getElementById('placeable')
+const buildings = document.getElementById('building')
+const guide = document.getElementById('guide')
+
+const topbar = document.getElementById('topbar')
+
+function checkForChange(item) {
+    
+}
+
+tools.addEventListener(
+    "change",
+    checkForChange
+)
