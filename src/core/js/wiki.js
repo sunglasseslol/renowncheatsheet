5a4bfa6db07857f74a257d7a157371477e00fbb8
sunglasses <fred@secondlast.com>
2022-12-13 22:34:19 +1100

    guide.html -> wiki.html, wiki.html dropdowns are working fine now , getting close to finishing, just need to fill out blank spots and get the js done

--- src/core/js/wiki.js
+++ src/core/js/wiki.js
@@ -4,10 +4,13 @@ const placeables = document.getElementById('placeable')
 const buildings = document.getElementById('building')
 const guide = document.getElementById('guide')
 
+// Tool Options
+const t1Pick = document.getElementById('t1Pick')
+
 const topbar = document.getElementById('topbar')
 
 function checkForChange(item) {
-    
+    let statsToDisplay = item
 }
 
 tools.addEventListener(
