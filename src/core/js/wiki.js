47ab00227075f0c4c3f81706c8373760b29c1482
sunglasses <fred@secondlast.com>
2022-12-21 20:06:09 +1100

    some of wiki.js boilerplate is done, trying to get githubpages to work

--- src/core/js/wiki.js
+++ src/core/js/wiki.js
@@ -6,25 +6,98 @@ const buildings = document.getElementById('building')
 // Tool Options
 const t1Pick = document.getElementById('t1Pick')
 
+// Weapon Options
+const t2Messer = document.getElementById('t2Messer')
+
+// Placeable Options
+const t1Furnace = document.getElementById('t1Furnace')
+
+// Building Options
+const t1Wall = document.getElementById('t1Wall')
+
+// Other
 const topbar = document.getElementById('topbar')
+const display = document.getElementById('display')
+
+function getHtmlForStats(elem) {
+    const baseImageLoc = "/misc/imgs/wiki/items/"
+    const item = elem
+    const itemId = item.id
+
+    for(_ = 0; _ < 5; _++) {console.log(itemId)}
+    
+    let imageLoc = `${baseImageLoc}` + `${itemId}` + '.png'
+
+    const baseHtml = `
+        <img id="statsImg" src="${imageLoc}">
+
+        <ul class="costList" id="stoneCostList">
+        Stone Tools
+            <li>Damage: <span style="color: #FFACE4"></span></li>
+            <li>Resource Gain: <span style="color: #FFACE4"></span></li>
+            <li>: <span style="color: #FFACE4"></span></li>
+            <br>
+            Requirements: <br>
+            <span style="color: #FFACE4">●</span> No Requirements
+        </ul>
+    `
+
+    display.innerHTML = baseHtml
+}
+
+function main() {
+    console.warn("wiki.js loaded")
+}
+
+main()
 
 function check() {
-    modifyClassStyle("dropdown-option", "dropdown-title", t1Pick)
+    // console.log('CHANGE')
+
+    displayTool()
+    getHtmlForStats(t2Messer)
 }
 
-function modifyClassStyle(className, idName, sel) {
-    let classNameToModify = document.getElementsByClassName(className)
-    let classToAvoid = document.getElementsByClassName(idName)
-    if(sel.className == classNameToModify) {
-        console.log('test')
-        let selId = sel.id
-        if(selId == idName) {
-            sel.style.color = "white";
-        }
-    }
+function displayTool() {
+    toolToDisplay = tools.value
+
+    // console.log(toolToDisplay)
+
+    let statWall = `${toolToDisplay}` + "Id"
+    // getHtmlForStats(t2Messer)
+
+    toolRemoveWhitespace = statWall.replace(/\s/g, '')
+    console.log(toolRemoveWhitespace)
 }
+//TEST FUNCTION MAY BE USED IN FUTURE
+function displayTools(type) {
+    itemToDisplay = type.value
+
+    // console.log(toolToDisplay)
+
+    let statWall = `${itemToDisplay}` + "Id"
+    getHtmlForStats()
+
+    itemRemoveWhitespace = statWall.replace(/\s/g, '')
+    // console.log(itemRemoveWhitespace)
+}
+
+weapons.addEventListener(
+    'change',
+    check
+)
 
 tools.addEventListener(
+    'change',
+    check
+)
+
+placeables.addEventListener(
+    'change',
+    check
+)
+
+buildings.addEventListener(
     'change',
     check
 )
---
+++ src/misc/global.js
@@ -0,0 +1,20 @@
+const backBtn = document.getElementById('backBtn')
+const resetBtn = document.getElementById('resetBtn')
+
+function backToIndex() {
+    location.href = "index.html"
+}
+
+function reset() {
+    location.reload()
+}
+
+// backBtn.addEventListener(
+//     'click',
+//     backToIndex
+// )
+
+// resetBtn.addEventListener(
+//     "click",
+//     reset
+// )
